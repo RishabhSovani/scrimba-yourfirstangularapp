3f2c14d8e5851f2ea804815706dd92eeb67f9b95
rssunstone <rishabh.sovani@sunstone.in>
2022-07-06 17:21:25 +0530

    app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scrimba-project';
+}
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-customers></app-customers>
+  `
+})
+export class AppComponent implements OnInit {
+  title : string = ''
+  constructor() { }
+
+  ngOnInit() {
+    this.title='Hello World using Data binding';
+    // We call a service that gets us the data
+  }
+
 }
